5cf3cdaba73f03df1357db99c5c362ac897dc0af
AZMIR AZIZ <azmir.aziz5197@gmail.com>
2025-08-21 19:25:25 +0800

    Compelete navbar components and page(contact,about,guest)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from "./components/Guest";
 
 
-export default function HomePage() {
+
+export default async function HomePage() {
+  const user = await currentUser();
+  if (!user) {
+    return (
+      <Guest />
+    )
+  }
   return (
     <div>
       <h1 className="bg-red-400">Home Page</h1>
